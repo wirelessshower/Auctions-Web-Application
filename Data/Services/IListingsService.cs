84dcf0ddf968cb5fb8c397b6670dac3d4bdf2e0e
Wireless Shower <139571097+wirelessshower@users.noreply.github.com>
2024-07-19 14:59:21 +0400

    First Commit
    7/19/2024

---
+++ Data/Services/IListingsService.cs
@@ -0,0 +1,12 @@
+﻿using Auctions.Models;
+
+namespace Auctions.Data.Services
+{
+    public interface IListingsService
+    {
+        IQueryable<Listing> GetAll();
+        Task Add(Listing listing);
+        Task<Listing> GetByID(int? id);
+        Task SaveChanges();
+    }
+}
